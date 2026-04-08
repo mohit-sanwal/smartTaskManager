90e79bc91e0d28b788b315748fad5b60967fb54a
mohitSanwal <mohit123ongraph@gmail.com>
2026-04-08 22:26:20 +0530

    HttpClient added and api integration for addtask and get task and behaviour subject used

--- src/app/features/tasks/task.service.ts
+++ src/app/features/tasks/task.service.ts
@@ -1,18 +1,25 @@
-import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { title } from "process";
 import { Observable, of } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class TaskService {
+    private http = inject(HttpClient);
+
+    private apiUrl =  'https://jsonplaceholder.typicode.com/todos'
     private tasks = ['Learn Angular', 'Build Project'];
 
-    getTasks(): Observable<string[]> {
-        return of(this.tasks);
+    getTasks(): Observable<any[]> {
+        return this.http.get<any[]>(this.apiUrl);
     }
 
-    addTask(task:string) : Observable<string[]> {
-        this.tasks.push(task);
-        return of(this.tasks);
+    addTask(task:string): Observable<any> {
+        return this.http.post(this.apiUrl, {
+            title: task,
+            completed: false
+        });
     }
 }
